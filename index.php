8fac8d776ccb1d9b4ea59ac1b9e19f72883fd5cb
Kv4tn <usupasanali@gmail.com>
2026-02-20 10:31:55 +0500

    Update index.php

--- index.php
+++ index.php
@@ -44,6 +44,15 @@ session_start();
             <p>Real-time flight data processing.</p>
         </div>
     </section>
-
+    <ul>
+    <li><a href="index.php">Home</a></li>
+    <?php if(isset($_SESSION['user_id'])): ?>
+        <li><a href="profile.php">My Bookings</a></li> <li><a href="booking.php">Book a Flight</a></li>
+        <li><a href="logout.php">Logout (<?php echo $_SESSION['username']; ?>)</a></li>
+    <?php else: ?>
+        <li><a href="login.php">Login</a></li>
+        <li><a href="register.php">Register</a></li>
+    <?php endif; ?>
+</ul>
 </body>
 </html>
